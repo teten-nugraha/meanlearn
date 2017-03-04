d411d84bde704760ff1e839d3b26d4609f3e7aaa
teten777 <teten.nugraha18@gmail.com>
2017-03-04 21:33:03 +0700

    install bcrypt dan user create proses

--- server.js
+++ server.js
@@ -24,7 +24,18 @@ app.post('/users',function(req,res) {
     user.username = req.body.username;
     user.password = req.body.password;
     user.email    = req.body.email;
-    user.save();
+
+    if(req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == ''){
+        res.json({ success:false,message:'Ensure username, email and password were provided' });
+    }else{
+        user.save(function(err){
+            if (err) {
+                res.json({ success:false,message:'Username or Email already exist' });
+            }else{
+                res.json({ success:true,message:'user created' });
+            }
+        });
+    }
 });
 
 app.listen(8080,function() {
