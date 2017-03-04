9f54b85c414bd1e7c9a53f5a28b8b9dbc053400d
teten777 <teten.nugraha18@gmail.com>
2017-03-04 22:13:16 +0700

    route to root

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var mongoose = require('mongoose'); //object modeling tool designed to work in a
 var bodyParser = require('body-parser'); // untuk membaca inputan json
 var router = express.Router();
 var appRoutes = require('./app/routes/api')(router);
+var path = require('path');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
+app.use(express.static(__dirname+'/public'));
 app.use('/api',appRoutes);
 
 mongoose.connect('mongodb://localhost:27017/meantutorial2',function(err) {
@@ -18,6 +20,10 @@ mongoose.connect('mongodb://localhost:27017/meantutorial2',function(err) {
     }else{
         console.log('Successfully connected to MongoDB');
     } 
+});
+
+app.get('*',function (req,res){
+	res.sendFile(path.join(__dirname+'/public/app/views/index.html'));
 });
 
 
