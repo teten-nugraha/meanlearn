0bd898467e59ae259bba09092eea62245f3dacc3
teten777 <teten.nugraha18@gmail.com>
2017-03-04 20:37:18 +0700

    mongodb install and connect

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
+var morgan = require('morgan'); //HTTP request logger middleware
+var mongoose = require('mongoose'); //object modeling tool designed to work in an asynchronous environment
 
-app.get('/',function(req,res){
-    res.send("Hello World");
+app.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost:27017/meantutorial2',function(err) {
+    if(err) {
+        console.log('Not connected to the database : '+err);
+    }else{
+        console.log('Successfully connected to MongoDB');
+    }
 });
 
 app.listen(8080,function() {
